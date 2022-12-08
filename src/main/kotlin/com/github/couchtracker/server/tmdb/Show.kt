20cf4c2a4e5054517e28d25a0b0a64301dac05ff
Mauro Molin <molin.mauro@gmail.com>
2022-12-08 14:52:38 +0000

    Make show download use coroutines

--- src/main/kotlin/com/github/couchtracker/server/tmdb/Show.kt
+++ src/main/kotlin/com/github/couchtracker/server/tmdb/Show.kt
@@ -1,43 +1,37 @@
 package com.github.couchtracker.server.tmdb
 
 import com.github.couchtracker.server.Config
+import com.github.couchtracker.server.common.makeNotNull
 import com.github.couchtracker.server.common.model.*
 import com.github.couchtracker.server.db.model.ShowDbo
 import com.uwetrottmann.tmdb2.entities.AppendToResponse
 import com.uwetrottmann.tmdb2.enumerations.AppendToResponseItem
+import retrofit2.await
 
-fun ShowDbo.Companion.downloadFromTmdb(tmdbId: Int): Result<ShowDbo> {
-    val tmdb = Config.Tmdb.client() ?: return Result.failure(IllegalStateException("Cannot download from TMDB: API key not set"))
+suspend fun ShowDbo.Companion.downloadFromTmdb(tmdbId: Int): ShowDbo {
+    val tmdb = Config.Tmdb.client() ?: throw IllegalStateException("Cannot download from TMDB: API key not set")
 
-    // TODO try to use enqueue to make function suspend
-    val result = tmdb.tvService()
+    val show = tmdb.tvService()
         .tv(
             tmdbId, "eng", AppendToResponse(
                 AppendToResponseItem.TRANSLATIONS,
                 AppendToResponseItem.EXTERNAL_IDS
             )
-        )
-        .execute()
+        ).makeNotNull()
+        .await()
 
-    if (!result.isSuccessful) {
-        return Result.failure(Exception(result.message()))
-    }
+    return ShowDbo(
+        id = TmdbId(tmdbId),
+        name = show.translations.toDbTranslations { it.name },
+        externalIds = ShowExternalIds(
+            tmdb = tmdbId.toLong(),
+            tvdb = show.external_ids.tvdb_id.toLong(),
+            imdb = show.external_ids.imdb_id,
+        ),
 
-    val show = result.body()!!
-
-    return Result.success(
-        ShowDbo(
-            id = TmdbId(tmdbId),
-            name = show.translations.toDbTranslations { it.name },
-            externalIds = ShowExternalIds(
-                tmdb = tmdbId.toLong(),
-                tvdb = show.external_ids.tvdb_id.toLong(),
-                imdb = show.external_ids.imdb_id,
-            ),
-
-            status = show.status?.let { ShowStatus.fromTmdbStatus(it) },
-            ratings = ShowRatings(
-                tmdb = Rating.Tmdb(show.vote_average, show.vote_count.toLong())
-            ),
-        ))
+        status = show.status?.let { ShowStatus.fromTmdbStatus(it) },
+        ratings = ShowRatings(
+            tmdb = Rating.Tmdb(show.vote_average, show.vote_count.toLong())
+        ),
+    )
 }
