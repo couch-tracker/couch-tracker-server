8ead7fc11342c4f49838b6bb4bf36298f58c0a3e
Mauro Molin <molin.mauro@gmail.com>
2022-12-15 09:24:07 +0000

    Add Resolution.Size.toClass() and make Resolution data classes

--- src/main/kotlin/com/github/couchtracker/server/model/Resolution.kt
+++ src/main/kotlin/com/github/couchtracker/server/model/Resolution.kt
@@ -9,7 +9,7 @@ import kotlinx.serialization.Serializable
 sealed class Resolution {
 
     @Serializable(with = Class.Serializer::class)
-    class Class(val enum: ResolutionClass) {
+    data class Class(val enum: ResolutionClass) {
 
         object Serializer : SingleFieldSerializer<Class, ResolutionClass>(
             name = "Resolution.Class",
@@ -20,10 +20,30 @@ sealed class Resolution {
     }
 
     @Serializable(with = Size.Serializer::class)
-    class Size(val width: Int, val height: Int) {
+    data class Size(val width: Int, val height: Int) {
         init {
             require(width > 0)
             require(height > 0)
+        }
+
+        fun toClass(): Class? {
+            // Mostly copied from: https://github.com/jellyfin/jellyfin/blob/2e4db18ebea51a3e0b2d9b822ccee3bad918173f/MediaBrowser.Model/Entities/MediaStream.cs#L595-L630
+            val cls = when {
+                width <= 256 && height <= 144 -> ResolutionClass.SD
+                width <= 426 && height <= 240 -> ResolutionClass.SD
+                width <= 640 && height <= 360 -> ResolutionClass.SD
+                width <= 682 && height <= 384 -> ResolutionClass.SD
+                width <= 720 && height <= 404 -> ResolutionClass.SD
+                width <= 854 && height <= 480 -> ResolutionClass.HQ
+                width <= 960 && height <= 544 -> ResolutionClass.HQ
+                width <= 1024 && height <= 576 -> ResolutionClass.HQ
+                width <= 1280 && height <= 962 -> ResolutionClass.HD
+                width <= 2560 && height <= 1440 -> ResolutionClass.FHD
+                width <= 4096 && height <= 3072 -> ResolutionClass.FOUR_K
+                width <= 8192 && height <= 8192 -> ResolutionClass.EIGHT_K
+                else -> null
+            }
+            return Class(cls ?: return null)
         }
 
         override fun toString() = "${width}x${height}"
