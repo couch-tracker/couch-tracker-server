77f73952af3a548efcab4f668cf676ed7b23f533
Mauro Molin <molin.mauro@gmail.com>
2022-12-15 21:50:04 +0000

    Add checks on Rating values

--- src/main/kotlin/com/github/couchtracker/server/model/Rating.kt
+++ src/main/kotlin/com/github/couchtracker/server/model/Rating.kt
@@ -3,11 +3,26 @@ package com.github.couchtracker.server.model
 import kotlinx.serialization.Serializable
 
 
-
 sealed class Rating {
     @Serializable
-    data class Tmdb(val value: Double, val count: Long) : Rating()
+    data class Tmdb(val average: Double, val count: Long) : Rating() {
+        init {
+            require(average >= 0)
+            require(count >= 0L)
+            if (count == 0L) {
+                require(average == 0.0)
+            }
+        }
+    }
 
     @Serializable
-    data class Tvdb(val value: Float, val count: Long) : Rating()
+    data class Tvdb(val value: Double, val count: Long) : Rating() {
+        init {
+            require(value >= 0)
+            require(count >= 0L)
+            if (count == 0L) {
+                require(value == 0.0)
+            }
+        }
+    }
 }
