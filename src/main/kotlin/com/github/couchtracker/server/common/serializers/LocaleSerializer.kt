e9cf2c91ead93413acb64ecd520c869ca85c65f5
Mauro Molin <molin.mauro@gmail.com>
2022-12-15 21:50:15 +0000

    Add LocaleSerializer

---
+++ src/main/kotlin/com/github/couchtracker/server/common/serializers/LocaleSerializer.kt
@@ -0,0 +1,12 @@
+package com.github.couchtracker.server.common.serializers
+
+import java.util.Locale
+
+object LocaleSerializer : RegexSerializer<Locale>(
+    name = "Locale",
+    regex = "([a-z]+)(?:[_\\-]([a-z]+))?".toRegex(RegexOption.IGNORE_CASE),
+    deserialize = { matcher -> Locale(
+        matcher.groupValues[1],
+        matcher.groupValues[2].takeIf { it.isNotBlank() }
+    ) }
+)
