9d79aeaeebdbcb5b62b77ce4d4ee546c7baa3f85
Mauro Molin <molin.mauro@gmail.com>
2023-02-16 20:40:47 +0000

    Add GET/PATCH/DELETE /users/{id} endpoints

--- src/main/kotlin/com/github/couchtracker/server/model/api/User.kt
+++ src/main/kotlin/com/github/couchtracker/server/model/api/User.kt
@@ -1,7 +1,14 @@
 package com.github.couchtracker.server.model.api
 
+import com.github.couchtracker.server.model.api.base.PatchObject
+import com.github.couchtracker.server.model.db.UserDbo
 import com.github.couchtracker.server.util.Email
+import com.github.couchtracker.server.util.OptionalField
+import com.github.couchtracker.server.util.OptionalField.Missing
+import com.github.couchtracker.server.util.Password
 import com.github.couchtracker.server.util.Username
+import com.github.couchtracker.server.util.setOptionals
+import com.github.couchtracker.server.util.setTo
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -10,4 +17,23 @@ data class User(
     val username: Username,
     val email: Email,
     val name: String,
+)
+
+@Serializable
+data class UserPatch(
+    val email: OptionalField<Email> = Missing,
+    val name: OptionalField<String> = Missing,
+) : PatchObject<UserDbo> {
+
+    override val dbo get() = UserDbo
+
+    override fun updateBson() = setOptionals(
+        UserDbo::email setTo email,
+        UserDbo::name setTo name,
+    )
+}
+
+@Serializable
+data class UserDelete(
+    val password: Password,
 )
---
+++ src/main/kotlin/com/github/couchtracker/server/routes/Users.kt
@@ -0,0 +1,64 @@
+package com.github.couchtracker.server.routes
+
+import com.github.couchtracker.server.ApplicationData
+import com.github.couchtracker.server.JWT
+import com.github.couchtracker.server.accessPrincipal
+import com.github.couchtracker.server.model.api.UserDelete
+import com.github.couchtracker.server.model.api.UserPatch
+import com.github.couchtracker.server.model.db.UserDbo
+import com.github.couchtracker.server.util.validate
+import io.ktor.http.HttpStatusCode
+import io.ktor.resources.Resource
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.application.call
+import io.ktor.server.auth.authenticate
+import io.ktor.server.request.receive
+import io.ktor.server.resources.delete
+import io.ktor.server.resources.get
+import io.ktor.server.resources.patch
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.util.pipeline.PipelineContext
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Resource("/users")
+private class Users {
+
+    @Serializable
+    @Resource("{id}")
+    data class Id(val parent: Users, val id: String)
+}
+
+fun Route.users(ad: ApplicationData) {
+    authenticate(JWT.Login.ACCESS) {
+        get<Users.Id> { (_, id) ->
+            val user = checkSelf(id)
+            call.respond(user.toApi())
+        }
+
+        patch<Users.Id> { (_, id) ->
+            val user = checkSelf(id)
+            val patch = call.receive<UserPatch>()
+            patch.update(ad.connection, user.id)
+            call.respond(HttpStatusCode.NoContent)
+        }
+
+        delete<Users.Id> { (_, id) ->
+            val user = checkSelf(id)
+            val (password) = call.receive<UserDelete>()
+            if (!ad.config.argon2.verify(user.password, password)) {
+                call.respond(HttpStatusCode.Unauthorized.description("Password is incorrect"))
+            } else {
+                UserDbo.collection(ad.connection).deleteOneById(user.id)
+                call.respond(HttpStatusCode.NoContent)
+            }
+        }
+    }
+}
+
+private suspend fun PipelineContext<Unit, ApplicationCall>.checkSelf(id: String): UserDbo {
+    val user = call.accessPrincipal.user
+    validate(user.id.toString() == id, HttpStatusCode.Forbidden)
+    return user
+}
