0bcc17c911841ad7b1a340f608bcb989e4c2f54a
Mauro Molin <molin.mauro@gmail.com>
2023-04-16 13:52:11 +0100

    Rename retrofit2.Call.makeNotNull to asNotNull and add test

--- src/main/kotlin/com/github/couchtracker/server/util/RetrofitExtensions.kt
+++ src/main/kotlin/com/github/couchtracker/server/util/RetrofitExtensions.kt
@@ -2,7 +2,10 @@ package com.github.couchtracker.server.util
 
 import retrofit2.Call
 
-fun <T : Any> Call<T?>.makeNotNull(): Call<T> {
+/**
+ * Performs an unchecked cast of the retrofit call to make it non-null
+ */
+fun <T : Any> Call<T?>.asNotNull(): Call<T> {
     @Suppress("UNCHECKED_CAST")
     return this as Call<T>
 }
