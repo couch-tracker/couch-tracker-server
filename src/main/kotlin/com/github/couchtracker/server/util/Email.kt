c91442fdd795473929ff22987e09861aeffe44f7
Mauro Molin <molin.mauro@gmail.com>
2023-04-16 13:20:37 +0100

    Use runValidations in Email init

--- src/main/kotlin/com/github/couchtracker/server/util/Email.kt
+++ src/main/kotlin/com/github/couchtracker/server/util/Email.kt
@@ -2,14 +2,18 @@ package com.github.couchtracker.server.util
 
 import kotlinx.serialization.Serializable
 
-private const val MIN_LENGTH = 3
-
 @Serializable
 @JvmInline
 value class Email(val value: String) {
 
     init {
-        require(value.length >= MIN_LENGTH) { "Email must have at least 3 characters" }
-        require('@' in value) { "Email must contain @ sign" }
+        runValidations {
+            validate(value.length >= MIN_LENGTH) { "Email must have at least $MIN_LENGTH characters" }
+            validate('@' in value) { "Email must contain @ sign" }
+        }.require()
+    }
+
+    companion object {
+        private const val MIN_LENGTH = 3
     }
 }
